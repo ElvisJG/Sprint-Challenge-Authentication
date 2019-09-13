15c9cdb93cbd931fc6251e608bef277a0ff7f0aa
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-13 19:37:34 -0400

    env and middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,34 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = {
+  generator,
+  restrict
 };
+
+function generator(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '8h'
+  };
+  return jwt.sign(payload, process.env.SECRET, options);
+}
+
+function restrict(req, res, next) {
+  const token = req.authorization.token;
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).json({ message: 'YOU SHALL NOT PASS! 🧙‍' });
+      } else {
+        req.decode;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: 'No token provided' });
+  }
+}
