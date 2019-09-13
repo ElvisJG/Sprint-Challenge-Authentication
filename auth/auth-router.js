1895ad37bf15e0cb9b3ab6481b6689e08c176e81
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-13 19:55:52 -0400

    adding models

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const middleware = require('./authenticate-middleware');
+const Users = require('../models/user-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  // Destructures body into user
+  const user = req.body;
+  // Takes users password and encrypts
+  user.password = bcrypt.hashSync(user.password, 12);
+  // Generates a token using the user
+  const token = middleware.generator(user);
 });
 
 router.post('/login', (req, res) => {
